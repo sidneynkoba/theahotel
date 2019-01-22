702579e6283819652935abcad4e9886ad5df549a
sidneynkoba <sidneyknightmark@gmail.com>
2019-01-22 23:32:30 +0300

    Initial Commit, created routing module, added hotel pages, added path

---
+++ src/app/weddings/thai-wedings/thai-wedings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thai-wedings',
+  templateUrl: './thai-wedings.component.html',
+  styleUrls: ['./thai-wedings.component.scss']
+})
+export class ThaiWedingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/weddings/westerns-wedings/westerns-wedings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-westerns-wedings',
+  templateUrl: './westerns-wedings.component.html',
+  styleUrls: ['./westerns-wedings.component.scss']
+})
+export class WesternsWedingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
